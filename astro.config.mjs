1ccd1affd904d23a315d3f26c376fb01e6bcc20b
littamir <60058914+littamir@users.noreply.github.com>
2026-05-23 17:10:43 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,44 +2,27 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import starlight from '@astrojs/starlight';
-
 import mdx from '@astrojs/mdx';
+import path from 'path';
 
-// https://astro.build/config
 export default defineConfig({
-  // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: 'https://screwfast.uk',
   image: {
     domains: ['images.unsplash.com'],
   },
-  // i18n: {
-  //   defaultLocale: "en",
-  //   locales: ["en", "fr"],
-  //   fallback: {
-  //     fr: "en",
-  //   },
-  //   routing: {
-  //     prefixDefaultLocale: false,
-  //   },
-  // },
   prefetch: true,
   integrations: [
     sitemap({
       i18n: {
-        defaultLocale: 'en', // All urls that don't contain language prefix will be treated as default locale
+        defaultLocale: 'en',
         locales: {
-          en: 'en', // The `defaultLocale` value must present in `locales` keys
+          en: 'en',
           fr: 'fr',
         },
       },
     }),
     starlight({
       title: 'ScrewFast Docs',
-      // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
-      // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
-      // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
-      // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
-      // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
       locales: {
         root: {
           label: 'English',
@@ -52,7 +35,6 @@ export default defineConfig({
         ja: { label: '日本語', lang: 'ja' },
         'zh-cn': { label: '简体中文', lang: 'zh-CN' },
       },
-      // https://starlight.astro.build/guides/sidebar/
       sidebar: [
         {
           label: 'Quick Start Guides',
@@ -95,8 +77,7 @@ export default defineConfig({
       components: {
         SiteTitle: './src/components/ui/starlight/SiteTitle.astro',
         Head: './src/components/ui/starlight/Head.astro',
-        MobileMenuFooter:
-          './src/components/ui/starlight/MobileMenuFooter.astro',
+        MobileMenuFooter: './src/components/ui/starlight/MobileMenuFooter.astro',
         ThemeSelect: './src/components/ui/starlight/ThemeSelect.astro',
       },
       head: [
@@ -123,5 +104,10 @@ export default defineConfig({
   },
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@images': path.resolve('./src/images'),
+      },
+    },
   },
 });
