f68487fef2230c81f319950f4b4b5ec15fe17823
littamir <60058914+littamir@users.noreply.github.com>
2026-05-23 12:51:08 +0330

    Update favicon.ico.ts

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -3,7 +3,7 @@ import sharp from 'sharp';
 import ico from 'sharp-ico';
 import path from 'node:path';
 
-const faviconSrc = path.resolve('public/icon.png');
+const faviconSrc = path.resolve('src/images/icon.png');
 
 export const GET: APIRoute = async () => {
   // Resize the image to multiple sizes
