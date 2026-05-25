8c87a80d7ee7ea3fdabada8ea65ceab2b529a706
littamir <60058914+littamir@users.noreply.github.com>
2026-05-25 17:47:06 +0330

    Update SITE tagline and URL in constants.ts

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -2,12 +2,12 @@ import ogImageSrc from '@images/social.png';
 
 export const SITE = {
   title: 'dairano',
-  tagline: 'Top-quality Hardware Tools',
+  tagline: 'Top-quality milk Powder',
   description:
     'ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.',
   description_short:
     'ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.',
-  url: 'https://dairano.com',
+  url: 'https://www.dairano.com/',
   author: 'Emil Gulamov',
 };
 
